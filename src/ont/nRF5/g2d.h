072fb4846d785da23503a42451218fdabe1b94c3
DuncanCragg <duncan@cilux.org>
2023-02-08 23:16:13 +0000

    remove old g2d kbd hackery

--- src/ont/nRF5/g2d.h
+++ src/ont/nRF5/g2d.h
@@ -16,17 +16,11 @@
 
 #define G2D_RGB256(r,g,b) ((((uint16_t)(r)&0xf8)<<8)|(((uint16_t)(g)&0xfc)<<3)|((uint16_t)(b)>>3))
 
-#define G2D_KEYBOARD_COMMAND_DELETE 0x01
-
-typedef void (*g2d_keyboard_cb)(unsigned char ch, uint8_t command);
-
 void g2d_init();
 void g2d_clear_screen(uint8_t colour);
                    // 8 bits * 2, so 0x00 => 0x0000, 0x77 => 0x7777, etc
 void g2d_text(int32_t x, int32_t y,
               char* text, uint16_t colour, uint16_t bg, uint32_t size);
-void g2d_keyboard(g2d_keyboard_cb kcb);
-void g2d_touch_event(uint16_t tx, uint16_t ty);
 void g2d_render();
 
 // ------ sprites

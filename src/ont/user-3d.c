043e5f4c0f14f940729dcc4f15a3f52199c31dd6
DuncanCragg <duncan@cilux.org>
2024-08-10 20:30:39 +0100

    bring in OK gamepad stuff

--- src/ont/user-3d.c
+++ src/ont/user-3d.c
@@ -23,11 +23,17 @@ static vec3  eye_l = { -eye_sep, 1.75, -5.0 };
 static vec3  eye_r = {  eye_sep, 1.75, -5.0 };
 static vec3  up = { 0.0f, -1.0, 0.0 };
 
-static float eye_dir=0;
+static float body_dir=0;
 static float head_hor_dir=0;
 static float head_ver_dir=0;
 
+static float xv = 0.0f;
+static float zv = 0.0f;
+
 void set_proj_view() {
+
+    eye_l[0] += xv; eye_r[0] += xv;
+    eye_l[2] += zv; eye_r[2] += zv;
 
     #define VIEWPORT_FOV   21.5f
     #define VIEWPORT_NEAR   0.1f
@@ -43,13 +49,13 @@ void set_proj_view() {
     vec3 looking_at_l;
     vec3 looking_at_r;
 
-    looking_at_l[0] = eye_l[0] + 100.0f * sin(eye_dir + eye_con + head_hor_dir);
+    looking_at_l[0] = eye_l[0] + 100.0f * sin(body_dir + eye_con + head_hor_dir);
     looking_at_l[1] = eye_l[1] - 100.0f * sin(                    head_ver_dir);
-    looking_at_l[2] = eye_l[2] + 100.0f * cos(eye_dir + eye_con + head_hor_dir);
+    looking_at_l[2] = eye_l[2] + 100.0f * cos(body_dir + eye_con + head_hor_dir);
 
-    looking_at_r[0] = eye_r[0] + 100.0f * sin(eye_dir - eye_con + head_hor_dir);
+    looking_at_r[0] = eye_r[0] + 100.0f * sin(body_dir - eye_con + head_hor_dir);
     looking_at_r[1] = eye_r[1] - 100.0f * sin(                    head_ver_dir);
-    looking_at_r[2] = eye_r[2] + 100.0f * cos(eye_dir - eye_con + head_hor_dir);
+    looking_at_r[2] = eye_r[2] + 100.0f * cos(body_dir - eye_con + head_hor_dir);
 
     mat4x4_look_at(view_l_matrix, eye_l, looking_at_l, up);
     mat4x4_look_at(view_r_matrix, eye_r, looking_at_r, up);
@@ -69,13 +75,49 @@ void ont_vk_iostate_changed() {
 
 #define LOG_IQ
 #ifdef LOG_IO
-  log_write("ont_vk_iostate_changed [%d,%d] @(%d %d) @(%f %f %f) buttons=(%d %d %d) key=%d\n",
-           io.mouse_x, io.mouse_y, io.mouse_scroll,
-           io.yaw, io.pitch, io.roll,
-           io.mouse_left, io.mouse_middle, io.mouse_right,
-           io.key);
+  log_write("ont_vk_iostate_changed D-pad=(%d %d %d %d) head=(%f %f %f) "
+            "joy 1=(%f %f) joy 2=(%f %f) "
+            "mouse pos=(%d %d %d) mouse buttons=(%d %d %d) key=%d\n",
+                   io.d_pad_left, io.d_pad_right, io.d_pad_up, io.d_pad_down,
+                   io.yaw, io.pitch, io.roll,
+                   io.joy_1_lr, io.joy_1_ud, io.joy_2_lr, io.joy_2_ud,
+                   io.mouse_x, io.mouse_y, io.mouse_scroll,
+                   io.mouse_left, io.mouse_middle, io.mouse_right,
+                   io.key);
 #endif
 
+  float sd = sin(body_dir);
+  float cd = cos(body_dir);
+  float sp = 0.025f;
+
+  xv = 0.0f; zv = 0.0f;
+
+  if(io.d_pad_up){
+    xv +=  sp * sd;
+    zv +=  sp * cd;
+  }
+  if(io.d_pad_down){
+    xv += -sp * sd;
+    zv += -sp * cd;
+  }
+  if(io.d_pad_right){
+    xv +=  sp * cd;
+    zv += -sp * sd;
+  }
+  if(io.d_pad_left){
+    xv += -sp * cd;
+    zv +=  sp * sd;
+  }
+
+  // ------
+
+  float bd = 3.1415926/200 * io.joy_2_lr;
+  body_dir += bd;
+
+  head_hor_dir=io.yaw;
+  head_ver_dir=io.pitch;
+
+/*
   bool bottom_left = io.mouse_x < swap_width / 3 && io.mouse_y > swap_height / 2;
 
   if(io.mouse_left && !body_moving && bottom_left){
@@ -93,12 +135,12 @@ void ont_vk_iostate_changed() {
     delta_x = dwell(delta_x, 0.0015f);
     delta_y = dwell(delta_y, 0.0015f);
 
-    eye_dir += 0.5f* delta_x;
+    body_dir += 0.5f* delta_x;
 
-    eye_l[0] += 4.0f * delta_y * sin(eye_dir);
-    eye_l[2] += 4.0f * delta_y * cos(eye_dir);
-    eye_r[0] += 4.0f * delta_y * sin(eye_dir);
-    eye_r[2] += 4.0f * delta_y * cos(eye_dir);
+    eye_l[0] += 4.0f * delta_y * sin(body_dir);
+    eye_l[2] += 4.0f * delta_y * cos(body_dir);
+    eye_r[0] += 4.0f * delta_y * sin(body_dir);
+    eye_r[2] += 4.0f * delta_y * cos(body_dir);
   }
   else
   if(!io.mouse_left && body_moving){
@@ -129,11 +171,7 @@ void ont_vk_iostate_changed() {
     head_hor_dir=0;
     head_ver_dir=0;
   }
-  else
-  if(io.yaw != 0.0f && io.pitch != 0.0f){
-    head_hor_dir=io.yaw;
-    head_ver_dir=io.pitch;
-  }
+*/
 }
 
 // ---------------------------------

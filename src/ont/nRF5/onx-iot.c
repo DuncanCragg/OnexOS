02cc14cce7068d7c59de7ffc4f82fade3d783ace
DuncanCragg <duncan@cilux.org>
2025-05-16 11:44:24 +0100

    evaluator shuffling

--- src/ont/nRF5/onx-iot.c
+++ src/ont/nRF5/onx-iot.c
@@ -15,33 +15,67 @@
 #include <onr.h>
 #include <ont.h>
 
+#define BATT_ADC_CHANNEL 0
 object* button;
+#if defined(BOARD_FEATHER_SENSE)
+object* battery;
+#endif
 object* light;
 #if defined(BOARD_FEATHER_SENSE)
 object* ledmx;
-object* battery;
 #endif
 
 static char* deviceuid;
 static char* buttonuid;
+#if defined(BOARD_FEATHER_SENSE)
+static char* batteryuid;
+#endif
 static char* lightuid;
 #if defined(BOARD_FEATHER_SENSE)
 static char* ledmxuid;
-static char* batteryuid;
+#endif
 
-static void every_10s(void*){
+#if defined(BOARD_FEATHER_SENSE)
+static void every_2s(void*){
   onex_run_evaluators(batteryuid, 0);
 }
 #endif
 
 static void button_changed(uint8_t pin, uint8_t type);
 
-bool evaluate_button_io(object* button, void* pressed);
-bool evaluate_light_io(object* light, void* d);
+static bool evaluate_light_out(object* light, void* d);
 #if defined(BOARD_FEATHER_SENSE)
-bool evaluate_ledmx_io(object* ledmx, void* d);
+static bool evaluate_ledmx_out(object* ledmx, void* d);
 // REVISIT: evaluators and behaviours sept
 #endif
+
+void set_up_gpio(){
+  gpio_init();
+#if defined(BOARD_PCA10059)
+  gpio_mode_cb(BUTTON_1, INPUT_PULLUP, RISING_AND_FALLING, button_changed);
+  gpio_mode(LED1_G, OUTPUT);
+  gpio_mode(LED2_B, OUTPUT);
+  gpio_set(LED1_G, LEDS_ACTIVE_STATE);
+  gpio_set(LED2_B, !LEDS_ACTIVE_STATE);
+#elif defined(BOARD_ITSYBITSY)
+  gpio_mode_cb(BUTTON_1, INPUT_PULLUP, RISING_AND_FALLING, button_changed);
+  gpio_mode(LED_1, OUTPUT);
+  gpio_set(LED_1, LEDS_ACTIVE_STATE);
+#elif defined(BOARD_FEATHER_SENSE)
+  gpio_mode_cb(BUTTON_1, INPUT_PULLUP, RISING_AND_FALLING, button_changed);
+  gpio_mode(LED_1, OUTPUT);
+  gpio_set(LED_1, !LEDS_ACTIVE_STATE);
+
+  led_matrix_init();
+  led_strip_init();
+  led_matrix_fill_rgb((colours_rgb){0, 16, 0});
+  led_strip_fill_rgb((colours_rgb){0, 16, 0});
+  led_matrix_show();
+  led_strip_show();
+
+  gpio_adc_init(BATTERY_V, BATT_ADC_CHANNEL);
+#endif
+}
 
 int main(){ // REVISIT: needs to be in OK and call up here like ont-vk
 
@@ -58,49 +92,22 @@ int main(){ // REVISIT: needs to be in OK and call up here like ont-vk
 #endif
 
   time_init();
+  random_init();
 
   log_init(config); // has: "if(debug_on_serial) serial_init(...);"
   if(debug_on_serial) serial_ready_state(); // blocks until serial stable
 
-  random_init();
-
-  gpio_init();
-#if defined(BOARD_PCA10059)
-  gpio_mode_cb(BUTTON_1, INPUT_PULLUP, RISING_AND_FALLING, button_changed);
-  gpio_mode(LED1_G, OUTPUT);
-  gpio_mode(LED2_B, OUTPUT);
-  gpio_set(LED1_G, LEDS_ACTIVE_STATE);
-  gpio_set(LED2_B, !LEDS_ACTIVE_STATE);
-#elif defined(BOARD_ITSYBITSY)
-  gpio_mode_cb(BUTTON_1, INPUT_PULLUP, RISING_AND_FALLING, button_changed);
-  gpio_mode(LED_1, OUTPUT);
-  gpio_set(LED_1, LEDS_ACTIVE_STATE);
-#elif defined(BOARD_FEATHER_SENSE)
-  gpio_mode_cb(BUTTON_1, INPUT_PULLUP, RISING_AND_FALLING, button_changed);
-  gpio_mode(LED_1, OUTPUT);
-  gpio_set(LED_1, !LEDS_ACTIVE_STATE);
-  led_matrix_init();
-  led_strip_init();
-  led_matrix_fill_rgb((colours_rgb){0, 16, 0});
-  led_strip_fill_rgb((colours_rgb){0, 16, 0});
-  led_matrix_show();
-  led_strip_show();
-#define ADC_CHANNEL 0
-  gpio_adc_init(BATTERY_V, ADC_CHANNEL);
-#endif
+  set_up_gpio();
 
   if(debug_on_serial){
     if(serial_ready_state() == SERIAL_POWERED_NOT_READY){
 #if defined(BOARD_FEATHER_SENSE)
-      led_matrix_fill_col("#100");
-      led_strip_fill_col("#100");
-      led_matrix_show();
-      led_strip_show();
+      led_strip_fill_col("#700");
+      led_matrix_fill_col("#200");
+      led_matrix_show(); led_strip_show();
 #endif
       log_flash(1,0,0);
-      time_delay_ms(400);
-      log_flash(1,0,0);
-
+      time_delay_ms(500);
       boot_reset(false);
     }
   }
@@ -111,27 +118,33 @@ int main(){ // REVISIT: needs to be in OK and call up here like ont-vk
 
   onex_init(config);
 
-  onex_set_evaluators("evaluate_button", evaluate_edit_rule, evaluate_button_io, 0);
-  onex_set_evaluators("evaluate_light",  evaluate_edit_rule, evaluate_light_logic, evaluate_light_io, 0);
+  onex_set_evaluators("evaluate_button", evaluate_edit_rule, evaluate_button_in, 0);
 #if defined(BOARD_FEATHER_SENSE)
-  onex_set_evaluators("evaluate_ledmx",   evaluate_edit_rule, evaluate_light_logic, evaluate_ledmx_io, 0);
   onex_set_evaluators("evaluate_battery", evaluate_battery_in, 0);
+#endif
+  onex_set_evaluators("evaluate_light",  evaluate_edit_rule, evaluate_light_logic, evaluate_light_out, 0);
+#if defined(BOARD_FEATHER_SENSE)
+  onex_set_evaluators("evaluate_ledmx",   evaluate_edit_rule, evaluate_light_logic, evaluate_ledmx_out, 0);
 #endif
   onex_set_evaluators("evaluate_device", evaluate_device_logic, 0);
 
-  button=object_new(0, "evaluate_button", "editable button", 4);
-  light =object_new(0, "evaluate_light",  "editable light", 4);
+  button =object_new(0, "evaluate_button", "editable button", 4);
+#if defined(BOARD_FEATHER_SENSE)
+  battery=object_new(0, "evaluate_battery", "battery", 4);
+#endif
+  light  =object_new(0, "evaluate_light",  "editable light", 4);
 #if defined(BOARD_FEATHER_SENSE)
   ledmx  =object_new(0, "evaluate_ledmx",   "editable light", 4);
-  battery=object_new(0, "evaluate_battery", "battery", 4);
 #endif
 
   deviceuid=object_property(onex_device_object, "UID");
   buttonuid=object_property(button,"UID");
+#if defined(BOARD_FEATHER_SENSE)
+  batteryuid =object_property(battery, "UID");
+#endif
   lightuid =object_property(light, "UID");
 #if defined(BOARD_FEATHER_SENSE)
   ledmxuid   =object_property(ledmx, "UID");
-  batteryuid =object_property(battery, "UID");
 #endif
 
   object_property_set(button, "name", "mango");
@@ -145,17 +158,18 @@ int main(){ // REVISIT: needs to be in OK and call up here like ont-vk
   object_set_evaluator(onex_device_object, "evaluate_device");
   object_property_set(onex_device_object, "name", "IoT");
   object_property_add(onex_device_object, "io", buttonuid);
+#if defined(BOARD_FEATHER_SENSE)
+  object_property_add(onex_device_object, "io", batteryuid);
+#endif
   object_property_add(onex_device_object, "io", lightuid);
 #if defined(BOARD_FEATHER_SENSE)
   object_property_add(onex_device_object, "io", ledmxuid);
-  object_property_add(onex_device_object, "io", batteryuid);
 #endif
 
   onex_run_evaluators(lightuid, 0);
 #if defined(BOARD_FEATHER_SENSE)
+  time_ticker(every_2s, 0, 2000);
   onex_run_evaluators(ledmxuid, 0);
-  onex_run_evaluators(batteryuid, 0);
-  time_ticker(every_10s, 0, 10000);
 #endif
 
   while(1){
@@ -172,13 +186,7 @@ static void button_changed(uint8_t pin, uint8_t type){
   onex_run_evaluators(buttonuid, (void*)button_pressed);
 }
 
-bool evaluate_button_io(object* button, void* pressed) {
-  char* s=(pressed? "down": "up");
-  object_property_set(button, "state", s);
-  return true;
-}
-
-bool evaluate_light_io(object* light, void* d) {
+bool evaluate_light_out(object* light, void* d) {
 
   if(object_property_is(light, "light", "on")){
 #if defined(BOARD_PCA10059)
@@ -201,7 +209,7 @@ bool evaluate_light_io(object* light, void* d) {
 }
 
 #if defined(BOARD_FEATHER_SENSE)
-bool evaluate_ledmx_io(object* ledmx, void* d) {
+bool evaluate_ledmx_out(object* ledmx, void* d) {
   if(object_property_is(ledmx, "light", "on")){
     led_matrix_fill_col(object_property(ledmx, "colour"));
     led_strip_fill_col(object_property(ledmx, "colour"));
